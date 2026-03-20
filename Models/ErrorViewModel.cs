c506e31c9426ade7e289a8225fc44ee23e3bc019
opilane <opilane@HP-CZC3217061>
2026-03-20 10:15:08 +0200

    Set up basics to use onward

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KaardiManguProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
