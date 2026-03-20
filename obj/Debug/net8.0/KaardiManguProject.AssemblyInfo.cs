7a08b2d743c4ffd98c60b4b75c334f6d25a4c294
Leib92 <allan.leibenau92@gmail.com>
2026-03-20 10:48:18 +0200

    Laying groundwork to start working on various pages/views. - Starting to modify Home Index - Added Accounts, Admin and Games views and index - Updated Privacy page

--- obj/Debug/net8.0/KaardiManguProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/KaardiManguProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KaardiManguProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c506e31c9426ade7e289a8225fc44ee23e3bc019")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+036b6f5908d2a5424bc75cfe2a1f5cce0ab1903f")]
 [assembly: System.Reflection.AssemblyProductAttribute("KaardiManguProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KaardiManguProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
