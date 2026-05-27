c7e60608c4cc5688c041cf5dbbf3e9fc900f5e4a
opilane <opilane@HP-CZC3217061>
2026-05-27 12:09:19 +0300

    Tried starting on this, dont know how ill be finishing this >->

--- obj/Debug/net8.0/KaardiManguProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/KaardiManguProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KaardiManguProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b4b198e6603648d5091acd79e39639abfb7877d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62020893ee65d2f93ee5a1d6f27a6cded8da6196")]
 [assembly: System.Reflection.AssemblyProductAttribute("KaardiManguProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KaardiManguProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
